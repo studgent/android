02c7e109d16678a204dcfc447f96974adb18b2a2
Ah-Lun Tang <tang@ahlun.be>
2014-03-16 17:44:18 +0100

    Issue #14: more get requests (by id, by name, per page)

--- src/be/ugent/oomo/groep12/studgent/utilities/CurlUtil.java
+++ src/be/ugent/oomo/groep12/studgent/utilities/CurlUtil.java
@@ -16,6 +16,13 @@ import be.ugent.oomo.groep12.studgent.exception.CurlException;
 import android.content.res.Resources;
 
 public class CurlUtil {
+	
+	/**
+	 * Executes a GET request to the resource specified in the parameter.
+	 * @param resource from the backend api
+	 * @return String with list of json items
+	 * @throws CurlException
+	 */
 	public static String get(String resource) throws CurlException {
 		String api = App.getContext().getResources().getString(R.string.studgent_api);
 		String target = api + '/' + resource;
@@ -38,5 +45,40 @@ public class CurlUtil {
 		} catch (Exception e) {
 			throw new CurlException(e);
 		}
+	}
+
+	
+	/**
+	 * GET request to the backend api to retrieve one object, specified by id.
+	 * @param resource of the backend api
+	 * @param id of the requested object
+	 * @return String with one json item
+	 * @throws CurlException
+	 */
+	public static String get_by_id(String resource, int id) throws CurlException{
+		return get(resource + "/id/" + id);
+	}
+
+	/**
+	 * GET request to the backend api to retrieve objects, filtered by name specified in the parameter
+	 * @param resource of the backend api
+	 * @param name to filter items on
+	 * @return String with filtered list with json items
+	 * @throws CurlException
+	 */
+	public static String get_by_name(String resource, String name) throws CurlException{
+		return get(resource + "/name/" + name);
+	}
+	
+	/**	
+	 * GET request to the backend api to retrieve objects per page
+	 * @param resource of the backend api
+	 * @param perpage amount of objects per page
+	 * @param page offset, pagenumber
+	 * @return String with page from list with json items
+	 * @throws CurlException
+	 */
+	public static String get_paged(String resource, int perpage, int page) throws CurlException{
+		return get(resource + "/page/" + perpage + '/' + page);
 	}
 }
