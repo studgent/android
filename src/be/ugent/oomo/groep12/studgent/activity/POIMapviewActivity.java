6c9f3fa37c8d010c452b8f2c72b709ef0ba1eacd
Wouter Deryckere <Wouter.Deryckere@ugent.be>
2014-03-16 15:02:45 +0100

    #10: add activity with basic mapview

---
+++ src/be/ugent/oomo/groep12/studgent/activity/POIMapviewActivity.java
@@ -0,0 +1,17 @@
+package be.ugent.oomo.groep12.studgent.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import be.ugent.oomo.groep12.studgent.R;
+
+
+public class POIMapviewActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_poi_mapview);
+	}
+	
+}
