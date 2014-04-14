8cb168cd91bac4e3aa5b2bfe141ba356f1b44150
Wouter Deryckere <Wouter.Deryckere@ugent.be>
2014-04-14 17:55:52 +0200

    issue #28: add quiz activity

--- src/be/ugent/oomo/groep12/studgent/activity/QuizActivity.java
+++ src/be/ugent/oomo/groep12/studgent/activity/QuizActivity.java
@@ -2,25 +2,16 @@ package be.ugent.oomo.groep12.studgent.activity;
 
 import be.ugent.oomo.groep12.studgent.R;
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
-public class ChallengeActivity extends Activity {
-
+public class QuizActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		this.overridePendingTransition(R.anim.animation_enter,
 				R.anim.animation_leave);
-		setContentView(R.layout.activity_challenge);
+		setContentView(R.layout.activity_quiz);
 	}
 	
-	public void openQuizActivity(View view){
-		Intent intent = new Intent(this, QuizActivity.class);
-		startActivity(intent);
-	}
-	
-
 }
