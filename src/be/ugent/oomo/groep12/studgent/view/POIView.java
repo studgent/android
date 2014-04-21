189385c98cc5a9a0e02a27dbd855e4de28ed5a17
BenjaminCottyn <benjamin@bpc.(none)>
2014-04-21 09:39:06 +0200

    Issue #30: Removed unneeded method call and added some info

--- src/be/ugent/oomo/groep12/studgent/view/POIView.java
+++ src/be/ugent/oomo/groep12/studgent/view/POIView.java
@@ -4,12 +4,11 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.util.Log;
 import android.view.View;
 import be.ugent.oomo.groep12.studgent.common.PointOfInterest;
 
-public class POIView extends View{
-	
+public class POIView extends View {
+
 	private PointOfInterest poi;
 	private Paint cpaint;
 	private Paint tpaint;
@@ -20,40 +19,40 @@ public class POIView extends View{
 		super(context);
 		init(context);
 	}
-	
+
 	public POIView(Context context, PointOfInterest poi) {
 		super(context);
 		init(context);
-		
+
 		this.poi = poi;
 	}
-	
+
 	private void init(Context context) {
-		
-		// Otherwise no onDraw() call
-		this.setWillNotDraw(false);
-		
+		// Paint object for POI marker
 		cpaint = new Paint();
 		cpaint.setAntiAlias(true);
 		cpaint.setColor(Color.GRAY);
 		cpaint.setStrokeWidth(7);
 		cpaint.setStyle(Paint.Style.STROKE);
 		
+		// Paint object for POI text
 		tpaint = new Paint();
 		tpaint.setAntiAlias(true);
 		tpaint.setColor(Color.GRAY);
 		tpaint.setTextSize(18.0f);
-		
+
 		screenWidth = context.getResources().getDisplayMetrics().widthPixels;
 		screenHeight = context.getResources().getDisplayMetrics().heightPixels;
-		
 	}
-	
+
 	@Override
 	public void onDraw(Canvas canvas) {
 		canvas.drawCircle(screenWidth / 2, screenHeight / 2, 35, cpaint);
-		int xPos = (int) ((screenWidth / 2) - (tpaint.measureText(poi.getName()) / 2 ));
-		int yPos = (int) ((screenHeight / 2) - ((tpaint.descent() + tpaint.ascent()) / 2)) + 48 ;
+		// Used to align POI name centered under the marker
+		int xPos = (int) ((screenWidth / 2) - (tpaint
+				.measureText(poi.getName()) / 2));
+		int yPos = (int) ((screenHeight / 2) - ((tpaint.descent() + tpaint
+				.ascent()) / 2)) + 48;
 		canvas.drawText(poi.getName(), xPos, yPos, tpaint);
 	}
 
