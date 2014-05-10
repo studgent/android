e167a48674761ee550bed97e7c8f793da09328a0
Wouter Deryckere <Wouter.Deryckere@ugent.be>
2014-05-10 19:34:17 +0200

    issue #28: 3 bugfixes datasource

--- src/be/ugent/oomo/groep12/studgent/data/QuizQuestionsDataSource.java
+++ src/be/ugent/oomo/groep12/studgent/data/QuizQuestionsDataSource.java
@@ -112,13 +112,15 @@ public class QuizQuestionsDataSource implements IDataSource {
 		LatLng location = new LatLng(latitude,longitude);
 		Date date_answered = null;
 		GregorianCalendar date = null;
-		boolean correct;
+		boolean correct=false;
 		if ( answered ) {
 			JSONObject last =  item.getJSONObject("last_answer");
 			date_answered = item.optString("date_from").equals("null") ? 
 								null :
-								new SimpleDateFormat("yyyy-MM-dd").parse(last.optString("date"));
-			date = new GregorianCalendar( date_answered.getYear(), date_answered.getMonth(), date_answered.getDay() );
+								new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(last.optString("date"));
+			date = new GregorianCalendar();
+			date.setTime(date_answered);
+			//date = new GregorianCalendar( date_answered.getYear(), date_answered.getMonth(), date_answered.getDay() );
 			correct = item.optBoolean("correct");
 		}
 		
@@ -128,14 +130,14 @@ public class QuizQuestionsDataSource implements IDataSource {
 		if ( choices_json.length() > 0 ) {
 			choices = new ArrayList<String>();
 			for (int i = 0; i < choices_json.length(); i++) {
-				choices.add(choices_json.getString(i));
+				choices.add(choices_json.getJSONObject(i).getString("choice"));
 			} 
 		}
 
 		quizquestion = new QuizQuestion(id, 
 										points, 
 										question, 
-										answered, 
+										correct, 
 										choices, 
 										answer, 
 										date , 
