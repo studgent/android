7af890f40425a8c72bfbe2d4954f1716716e5b44
Wouter Deryckere <Wouter.Deryckere@ugent.be>
2014-05-13 13:44:56 +0200

    issue #20: locationUtil comments

--- src/be/ugent/oomo/groep12/studgent/utilities/iDistanceUpdatedListener.java
+++ src/be/ugent/oomo/groep12/studgent/utilities/iDistanceUpdatedListener.java
@@ -2,7 +2,7 @@ package be.ugent.oomo.groep12.studgent.utilities;
 
 import android.location.Location;
 
-public interface iDistanceUpdatedListener {
+public interface IDistanceUpdatedListener {
 	public void distanceIsUpdated();
 }
 
