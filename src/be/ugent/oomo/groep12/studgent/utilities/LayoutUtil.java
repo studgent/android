3eed0cf5e2b17dca2678ab97622615ceacad93cd
Ah-Lun Tang <tang@ahlun.be>
2014-05-13 10:11:56 +0200

    untested static scrollview fix http://cl.ly/image/1A2y0y1J1v1L

--- src/be/ugent/oomo/groep12/studgent/utilities/LayoutUtil.java
+++ src/be/ugent/oomo/groep12/studgent/utilities/LayoutUtil.java
@@ -1,11 +1,22 @@
 package be.ugent.oomo.groep12.studgent.utilities;
 
+import java.util.ArrayList;
+import java.util.Map;
+
+import be.ugent.oomo.groep12.studgent.R;
+import be.ugent.oomo.groep12.studgent.activity.POIListActivity;
+import be.ugent.oomo.groep12.studgent.adapter.POIAdapter;
+import be.ugent.oomo.groep12.studgent.common.IPointOfInterest;
+import be.ugent.oomo.groep12.studgent.common.PointOfInterest;
+import be.ugent.oomo.groep12.studgent.data.POIDataSource;
+import android.os.AsyncTask;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.app.ProgressDialog;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 
@@ -32,21 +43,44 @@ public class LayoutUtil {
 	}
 	
 	public static void updateListViewHeight(ListView myListView) {
-	     ListAdapter myListAdapter = myListView.getAdapter();
-	     if (myListAdapter == null) {            
-	              return;
-	     }
-	    //get listview height
-	    int totalHeight = 0;
-	    int adapterCount = myListAdapter.getCount();
-	    for (int size = 0; size < adapterCount ; size++) {
-	        View listItem = myListAdapter.getView(size, null, myListView);
-	        listItem.measure(0, 0);
-	        totalHeight += listItem.getMeasuredHeight();
-	    }
-	    //Change Height of ListView 
-	    ViewGroup.LayoutParams params = myListView.getLayoutParams();
-	    params.height = totalHeight + (myListView.getDividerHeight() * (adapterCount - 1));
-	    myListView.setLayoutParams(params);
+		new AsyncPOIListViewLoader().execute(myListView);
+	}
+	
+
+
+	protected static class AsyncPOIListViewLoader extends
+			AsyncTask<ListView, Void, ViewGroup.LayoutParams> {
+		
+		private ListView myListView;
+
+		@Override
+		protected void onPostExecute(ViewGroup.LayoutParams result) {
+			super.onPostExecute(result);
+		    myListView.setLayoutParams(result);
+		}
+
+
+		@Override
+		protected ViewGroup.LayoutParams doInBackground(
+				ListView... params) {
+			myListView = params[0];
+		     ListAdapter myListAdapter = myListView.getAdapter();
+		     if (myListAdapter != null) {
+			    //get listview height
+			    int totalHeight = 0;
+			    int adapterCount = myListAdapter.getCount();
+			    for (int size = 0; size < adapterCount ; size++) {
+			        View listItem = myListAdapter.getView(size, null, myListView);
+			        listItem.measure(0, 0);
+			        totalHeight += listItem.getMeasuredHeight();
+			    }
+			    //Change Height of ListView 
+			    ViewGroup.LayoutParams layoutparams = myListView.getLayoutParams();
+			    layoutparams.height = totalHeight + (myListView.getDividerHeight() * (adapterCount - 1));
+			    return layoutparams;
+		    } else {
+		    	return myListView.getLayoutParams();
+		    }
+		}
 	}
 }
